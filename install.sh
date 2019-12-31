ae19a18c3bdb90fa5ab1fb950fca0c79eeab4764
Marco <marco+github@bytes-delivery.de>
2019-12-31 07:57:50 +0100

    Update install.sh
    * redesign the script to use target WSL2

--- install.sh
+++ install.sh
@@ -1,57 +1,91 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
-    echo "Enter your username: "
-    read user
-else
-    user=$1
-fi
-
-if [ -z "$2" ]
-then
-    echo "Enter your master password: "
-    read -s password
-else
-    password=$2
-fi
-
-if [ -z "$3" ]
-then
-    echo "Do you want to skip rankmirrors (faster upfront)? [y/N] "
-    read response
-    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-    then
-        fast=1
-    else
-        fast=0
+# function
+global_settings()  {
+    if [ -z "$user" ]; then
+        echo "Enter your username: "
+        read user
     fi
-else
-    fast=$3
-fi
 
-# set time
-timedatectl set-ntp true
+    if [ -z "$password" ]; then
+        echo "Enter your master password: "
+        read -s password
+    fi
+        
+    if [ -z "$fast" ]; then
+        echo "Do you want to skip rankmirrors (faster upfront)? [y/N] "
+        read response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+        then
+            fast=1
+        else
+            fast=0
+        fi
+    fi
 
-#partiton disk
-parted --script /dev/sda mklabel msdos mkpart primary ext4 0% 87% mkpart primary linux-swap 87% 100%
-mkfs.ext4 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mount /dev/sda1 /mnt
+    # set time
+    timedatectl set-ntp true
 
-# pacstrap
-pacstrap /mnt base
+} # END: global_settings
 
-# fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-echo "org /home/$user/org vboxsf uid=$user,gid=wheel,rw,dmode=700,fmode=600,nofail 0 0" >> /mnt/etc/fstab
-echo "workspace /home/$user/workspace vboxsf uid=$user,gid=wheel,rw,dmode=700,fmode=600,nofail 0 0" >> /mnt/etc/fstab
+use_virtualbox() {
 
-# chroot
-wget https://raw.githubusercontent.com/abrochard/spartan-arch/master/chroot-install.sh -O /mnt/chroot-install.sh
-arch-chroot /mnt /bin/bash ./chroot-install.sh $user $password $fast
+    #partiton disk
+    parted --script /dev/sda mklabel msdos mkpart primary ext4 0% 87% mkpart primary linux-swap 87% 100%
+    mkfs.ext4 /dev/sda1
+    mkswap /dev/sda2
+    swapon /dev/sda2
+    mount /dev/sda1 /mnt
 
-# reboot
-umount /mnt
-reboot
+    # pacstrap
+    pacstrap /mnt base
+
+    # fstab
+    genfstab -U /mnt >> /mnt/etc/fstab
+    echo "org /home/$user/org vboxsf uid=$user,gid=wheel,rw,dmode=700,fmode=600,nofail 0 0" >> /mnt/etc/fstab
+    echo "workspace /home/$user/workspace vboxsf uid=$user,gid=wheel,rw,dmode=700,fmode=600,nofail 0 0" >> /mnt/etc/fstab
+
+    _chroot_install   
+   
+    # reboot
+    umount /mnt
+    reboot
+} # END: use_virtualbox
+
+use_wsl2(){
+   _chroot_install
+} # END: use_wsl2
+
+_chroot_install(){
+    # chroot
+    wget https://raw.githubusercontent.com/abrochard/spartan-arch/master/chroot-install.sh -O /mnt/chroot-install.sh
+    arch-chroot /mnt /bin/bash ./chroot-install.sh $user $password $fast
+} # END: _chroot_install
+
+# main
+
+# default
+user=""
+password=""
+fast="y"
+target="wsl2"
+
+# arguments                                                      TODO: use getops
+user="$1"
+password="$2"
+fast="$3"
+target="$4"
+
+echo "Please chose your target: [1]: VirtualBox; [2]: WSL2"
+read target
+
+case $target in
+    "1"|"vbox"|"virtualbox")
+        global_settings ;;
+        use_virtualbox ;;
+    "2"|"wsl2")
+        global_settings ;;
+        use_wsl2 ;;
+    *)
+       echo "${target} is not implemented yet, feel free to send a patch" ;;
+esac
