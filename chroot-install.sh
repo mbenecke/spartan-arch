92e568aeb77bfafd6e6fd59836664971f5ea700e
Marco <marco+github@bytes-delivery.de>
2020-03-21 16:35:36 +0100

    Update chroot-install.sh
    like to reach the useless use of cat award

--- chroot-install.sh
+++ chroot-install.sh
@@ -95,7 +95,7 @@ pacman -S --needed --noconfirm aria2c bind-tools mtr liboping ranger jqv colordi
 pacman -S --needed --noconfirm lynis nethogs nmon reptyr broot
 #do a Installation from the package.txt, which is a clone from a golden source
 wget https://raw.githubusercontent.com/mbenecke/spartan-arch/master/packages.txt -o /home/$user/package.txt
-pacman -S --needed --noconfirm < /home/$user/packages.txt
+pacman -S --needed --noconfirm < $( cat /home/$user/packages.txt )
 npm install -g jscs jshint bower grunt
 pip install pipenv bpython ipython
 pip install pytest nose black pyflakes isort 
